68ccfc881dac9be41d95312e04b258222728ccf5
Joao Vitor S. Rezende <jvsrpassos@gmail.com>
2022-04-21 19:30:50 -0300

    Novo knn

--- knn.py
+++ knn.py
@@ -1,4 +1,5 @@
 import os
+from matplotlib.cbook import print_cycles
 import pandas as pd
 import numpy as np
 import operator
@@ -27,7 +28,7 @@ def knn(dataset, testInstance, k):
     counts = {"Iris-setosa" : 0, "Iris-versicolor" : 0, "Iris-virginica" : 0}
     # Computing the most frequent class
     for x in range(len(neighbors)):
-        response = dataset.iloc[neighbors[x]][-1] 
+        response = dataset.iloc[neighbors[x]][-1]
         if response in counts:
             counts[response] += 1
         else:
@@ -36,7 +37,7 @@ def knn(dataset, testInstance, k):
     sort_counts = sorted(counts.items(), key=operator.itemgetter(1), reverse=True)
     return(sort_counts[0][0])
 
-data = pd.read_csv(os.path.join("C:\\Users", "Joao", "Documents", "Python Codes", "knn", "iris.csv"), names=['id', 'sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'specie'])
+data = pd.read_csv(os.path.join("./", "iris.csv"), names=['id', 'sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'specie'])
 
 indexes = np.random.permutation(data.shape[0])
 div = int(0.75 * len(indexes))
@@ -50,13 +51,12 @@ dev_class = list(development_set.iloc[:,-1])
 
 # Criando uma lista com todas as colunas da lista original com exceção da espécie
 row_list = []
-for index, rows in development_set.iterrows():
-    my_list =[rows.sepal_length, rows.sepal_width, rows.petal_length, rows.petal_width]       
+for index, rows in test_set.iterrows():
+    my_list = [rows.sepal_length, rows.sepal_width, rows.petal_length, rows.petal_width]       
     row_list.append([my_list])
     
 # Diferentes K para testar
-# k_n = [1, 3, 5, 7]
-k_n = [1, 3]
+k_n = [1, 3, 5, 7]
 development_set_obs_k = {}
 for k in k_n:
     development_set_obs = []
@@ -65,12 +65,10 @@ for k in k_n:
     development_set_obs_k[k] = development_set_obs
 # Dicionário contendo a classe para cada k
 obs_k = development_set_obs_k
-# print(obs_k)
 
 # Calculando a taxa de reconhecimento 
 rate = {}
 for k_value in obs_k.keys():
-    #print('k = ', key)
     count = 0
     for i,j in zip(dev_class, obs_k[k_value]):
         if i == j:
@@ -78,27 +76,7 @@ for k_value in obs_k.keys():
         else:
             pass
     rate[k_value] = count/(len(dev_class))
+    print("K = " + str(k_value) + " / Taxa de reconhecimento = " + str(rate[k_value]))
 
-df_res = pd.DataFrame({'k': k_n})
-value = list(rate.values())
-df_res = value
-# print(df_res)
-
-# Creating a list of list of all columns except 'class' by iterating through the development set
-row_list_test = []
-for index, rows in test_set.iterrows(): 
-    my_list =[rows.sepal_length, rows.sepal_width, rows.petal_length, rows.petal_width]       
-    row_list_test.append([my_list])
-test_set_obs = []
-for i in range(len(row_list_test)):
-    test_set_obs.append(knn(test_set, pd.DataFrame(row_list_test[i]), 3)) # No lugar de 3, deve ser o melhor K
-#print(test_set_obs)
-
-count = 0
-for i,j in zip(test_class, test_set_obs):
-    if i == j:
-        count = count + 1
-    else:
-        pass
-accuracy_test = count/(len(test_class))
-print('Final Accuracy of the Test dataset is ', accuracy_test)
+# for i in range(k):
+#     print()
---
+++ new_knn.py
@@ -0,0 +1,80 @@
+import os, operator
+import pandas as pd
+import numpy as np
+
+def euclideanDistance(data, data_, data_len):
+    dist = 0
+    for i in range(data_len):
+        dist = dist + np.square(data[i] - data_[i])
+    return np.sqrt(dist)
+
+def knn(dataset, testset, k):
+    distances = {}
+    # Quantidade de dados para calcular a distância
+    length = testset.shape[1]
+
+    # Repetição com os dados do dataset (base)
+    for i in range(len(dataset)):
+        # Calculando a distância do dado de teste com a base
+        distance = euclideanDistance(testset, dataset.iloc[i], length)
+        distances[i] = distance[0]
+
+    # Ordenando o array de distâncias
+    sort_distances = sorted(distances.items(), key=operator.itemgetter(1))
+
+    # Buscando K vizinhos mais próximos
+    neighbors = []
+    for i in range(k):
+        neighbors.append(sort_distances[i][0])
+
+    # Lista para contagem das espécies dos vizinhos mais próximos
+    counts = {"Iris-setosa" : 0, "Iris-versicolor" : 0, "Iris-virginica" : 0}
+
+    for x in range(len(neighbors)):
+        response = dataset.iloc[neighbors[x]][-1]
+        counts[response] += 1
+
+    #Ordenando a contagem de espécies 
+    sort_counts = sorted(counts.items(), key=operator.itemgetter(1), reverse=True)
+    return sort_counts[0][0]
+
+data = pd.read_csv(os.path.join("./", "iris.csv"), names=['id', 'sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'specie'])
+
+#Embaralhando os índices para dividir entre a base e os testes
+indexes = np.random.permutation(data.shape[0])
+
+#Definido um corte de 80%, ou seja, 80% do dados fornecidos serão de base e o resto de teste
+cut = int(0.8 * len(indexes))
+base_id, test_id = indexes[:cut], indexes[cut:]
+baseset, test_data = data.loc[base_id,:], data.loc[test_id,:]
+
+test_specie = list(test_data.iloc[:,-1])
+# print(test_class)
+
+# Criando uma lista apenas com os dados de teste
+testset = []
+for index, rows in test_data.iterrows():
+    testset.append([[rows.sepal_length, rows.sepal_width, rows.petal_length, rows.petal_width]])
+
+k_values = [1, 7]
+for k in k_values:
+    counts = {
+        "Iris-setosa" : {"Iris-setosa" : 0, "Iris-versicolor" : 0, "Iris-virginica" : 0}, 
+        "Iris-versicolor" : {"Iris-setosa" : 0, "Iris-versicolor" : 0, "Iris-virginica" : 0}, 
+        "Iris-virginica" : {"Iris-setosa" : 0, "Iris-versicolor" : 0, "Iris-virginica" : 0}
+    }
+    correct = 0
+    for i in range(len(testset)):
+        specie_result = knn(baseset, pd.DataFrame(testset[i]), k)
+        print(specie_result)
+        counts[test_specie[i]][specie_result] += 1
+        if (specie_result == test_specie[i]):
+            correct += 1
+
+    print("K = " + str(k) + " / Taxa de reconhecimento = " + str(correct * 100 / len(testset)))
+    print("Matriz de confusão")
+    print("                  Iris setosa | Iris versicolor | Iris virginica")
+    print("Iris setosa     |          " + str(counts['Iris-setosa']['Iris-setosa']) + " |               " + str(counts['Iris-setosa']['Iris-versicolor']) + " |              " + str(counts['Iris-setosa']['Iris-virginica']) + "")
+    print("Iris versicolor |          " + str(counts['Iris-versicolor']['Iris-setosa']) + " |               " + str(counts['Iris-versicolor']['Iris-versicolor']) + " |              " + str(counts['Iris-versicolor']['Iris-virginica']) + "")
+    print("Iris virginica  |          " + str(counts['Iris-virginica']['Iris-setosa']) + " |               " + str(counts['Iris-virginica']['Iris-versicolor']) + " |              " + str(counts['Iris-virginica']['Iris-virginica']) + "")
+    # print(counts)
